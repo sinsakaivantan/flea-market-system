845b4e8df7864cbdd57b8e1d40d945b6ed5d80f7
a <a@localhost>
2026-01-05 00:33:04 +0900

    z

--- src/main/java/com/example/fleamarketsystem/entity/AppOrder.java
+++ src/main/java/com/example/fleamarketsystem/entity/AppOrder.java
@@ -11,11 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//aaaazzzz
 @Entity
 @Table(name = "app_order")
 @Data
