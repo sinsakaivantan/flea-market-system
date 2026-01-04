ca4a039fadbcd80aa1cce8544e2bdab1d70de106
a <a@localhost>
2026-01-05 00:46:04 +0900

    a

---
+++ src/main/java/com/example/fleamarketsystem/entity/Admin.java
@@ -0,0 +1,41 @@
+package com.example.fleamarketsystem.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+	
+	@Column(nullable = false)
+	private int action = 0;
+	
+	@Column(columnDefinition = "TEXT")
+    private String description;
+	
+	@Column(name = "time", nullable = false) 
+    private LocalDateTime time = LocalDateTime.now();
+	
+}
